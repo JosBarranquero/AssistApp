2aa1eab5d602fe921541244ba2ddb7c64aee54a9
José Antonio Barranquero <joseantbarranquero@gmail.com>
2016-11-07 19:33:04 +0100

    First commit

---
+++ app/src/main/java/com/bitbits/assistapp/Login_Activity.java
@@ -0,0 +1,18 @@
+package com.bitbits.assistapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * Class which will manage the system login
+ * @author José Antonio Barranquero Fernández
+ * @version 1.0
+ */
+public class Login_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
