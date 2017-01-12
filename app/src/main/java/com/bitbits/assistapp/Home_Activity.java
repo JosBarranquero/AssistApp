f145f447fd5d029cb9739639aa4de11c9da9e5f9
José Antonio Barranquero <joseantbarranquero@gmail.com>
2017-01-12 17:23:11 +0100

    Navigation Drawer working, needs improving

--- app/src/main/java/com/bitbits/assistapp/Home_Activity.java
+++ app/src/main/java/com/bitbits/assistapp/Home_Activity.java
@@ -1,8 +1,14 @@
 package com.bitbits.assistapp;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 /**
  * Activity which will show the Fragments that compose this Application
@@ -15,13 +21,63 @@ public class Home_Activity extends AppCompatActivity implements ConversationList
     MedicalRecord_Fragment mMedicalRecordFragment;
     Settings_Fragment mSettingsFragment;
 
+    private Toolbar mToolbar;
+    private DrawerLayout mDrawerLayout;
+    private NavigationView mNavigationView;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.content_nav);
 
+        mToolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_menu_home);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        mDrawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
+
+        mNavigationView = (NavigationView)findViewById(R.id.navigation_view);
+
+        setupDrawer();
+
         mConversationListFragment = new ConversationList_Fragment();
         getFragmentManager().beginTransaction().add(R.id.framehome, mConversationListFragment).commit();
+    }
+
+    private void setupDrawer() {
+        mNavigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                item.setChecked(true);
+                switch (item.getItemId()) {
+                    case R.id.navHistory:
+                        showMedicalRecord();
+                        break;
+                    case R.id.navSettings:
+                        showSettings();
+                        break;
+                    case R.id.navLogout:
+                        logOut();
+                        break;
+                    default:
+                        item.setChecked(false);
+                        break;
+                }
+                mDrawerLayout.closeDrawer(GravityCompat.START);
+                return true;
+            }
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
